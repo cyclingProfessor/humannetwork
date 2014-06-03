542c616a3d437dba7047d253dfad787417010371
Pierre-Malo Deniélou <malo@doc.ic.ac.uk>
2014-06-03 10:17:35 -0700

    add tooltips

--- src/ClientGui.java
+++ src/ClientGui.java
@@ -48,10 +48,12 @@ public class ClientGui {
 		scrollPane.setViewportView(list);
 
 		JButton btnAdd = new JButton("Add");
+		btnAdd.setToolTipText("Press to add the message content to the list of messages");
 		btnAdd.setBounds(570, 43, 212, 25);
 		frmDarknet.getContentPane().add(btnAdd);
 		
 		JButton btnSend = new JButton("Send");
+		btnSend.setToolTipText("Press to send the current message to someone on the network");
 		btnSend.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 			}
@@ -60,18 +62,22 @@ public class ClientGui {
 		frmDarknet.getContentPane().add(btnSend);
 		
 		JButton btnSplit = new JButton("Split");
+		btnSplit.setToolTipText("Press to split the message into small chunks");
 		btnSplit.setBounds(570, 264, 212, 25);
 		frmDarknet.getContentPane().add(btnSplit);
 		
 		JButton btnMerge = new JButton("Merge");
+		btnMerge.setToolTipText("Press to merge the selected messages");
 		btnMerge.setBounds(570, 227, 212, 25);
 		frmDarknet.getContentPane().add(btnMerge);
 		
 		JButton btnAddCheck = new JButton("Add Checksum");
+		btnAddCheck.setToolTipText("Press to add a checksum at the end of the current messsage");
 		btnAddCheck.setBounds(570, 339, 212, 25);
 		frmDarknet.getContentPane().add(btnAddCheck);
 		
 		JButton btnDelete = new JButton("Delete");
+		btnDelete.setToolTipText("Press to delete the selected messages");
 		btnDelete.setBounds(570, 536, 212, 25);
 		frmDarknet.getContentPane().add(btnDelete);
 		
@@ -80,26 +86,32 @@ public class ClientGui {
 		frmDarknet.getContentPane().add(lblMessage);
 
 		JButton btnNonce = new JButton("Add Random number");
+		btnNonce.setToolTipText("Press to add a random number to the current message");
 		btnNonce.setBounds(570, 487, 212, 25);
 		frmDarknet.getContentPane().add(btnNonce);
 		
 		JButton btnVerifyChecksum = new JButton("Verify Checksum");
+		btnVerifyChecksum.setToolTipText("Press to verify if the checksum of the current message is correct");
 		btnVerifyChecksum.setBounds(570, 376, 212, 25);
 		frmDarknet.getContentPane().add(btnVerifyChecksum);
 		
 		JButton btnMoveUp = new JButton("Move Up");
+		btnMoveUp.setToolTipText("Press to make the currently selected message move up in the list");
 		btnMoveUp.setBounds(570, 116, 212, 25);
 		frmDarknet.getContentPane().add(btnMoveUp);
 		
 		JButton btnMoveDown = new JButton("Move Down");
+		btnMoveDown.setToolTipText("Press to make the currently selected message move down in the list");
 		btnMoveDown.setBounds(570, 153, 212, 25);
 		frmDarknet.getContentPane().add(btnMoveDown);
 		
 		JButton btnEncrypt = new JButton("Encrypt");
+		btnEncrypt.setToolTipText("Press to encrypt the current message");
 		btnEncrypt.setBounds(570, 413, 212, 25);
 		frmDarknet.getContentPane().add(btnEncrypt);
 		
 		JButton btnDecrypt = new JButton("Decrypt");
+		btnDecrypt.setToolTipText("Press to decrypt the current message");
 		btnDecrypt.setBounds(570, 450, 212, 25);
 		frmDarknet.getContentPane().add(btnDecrypt);
 		
