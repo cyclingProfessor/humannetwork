49d8a67dfe5ce165c48d937992cbeaa8b75571cd
Dave Cohen <dave@stafflinux.cs.rhul.ac.uk>
2017-02-22 11:08:14 +0000

    Removed the old NetworkTest.java for now - it will be back.

--- Server/test/AllTests.java
+++ Server/test/AllTests.java
@@ -3,7 +3,7 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({ RouteTest.class, TextsTest.class, NetworkTest.class })
+@SuiteClasses({ RouteTest.class, TextsTest.class})
 public class AllTests {
 
 }
